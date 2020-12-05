2df6a2d6f2d93a28bb735464883ed3c87aa70fb6
dev stefanCho <61320923+devstefancho@users.noreply.github.com>
2020-12-05 21:31:01 +0900

    redux-logger middleware redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
+import logger from "redux-logger";
 
 import rootReducer from "./reducers";
 
-export default createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export default createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
